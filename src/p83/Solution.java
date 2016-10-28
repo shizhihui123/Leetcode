4049fd59c6b58aab0918fd53ed8596d57ba3169c
c0de8ug <c0de8ug@gmail.com>
2016-10-28 14:15:55 +0800

    solved p83
    solved p83

--- src/p83/Solution.java
+++ src/p83/Solution.java
@@ -1,11 +1,34 @@
 package p83;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         HashMap map = new HashMap();
-        map.put(,);
+        if (head == null){
+            return head;
+        }
+        map.put(head.val,new Object());
+        ListNode prev = head;
+        ListNode node = head.next;
+
+        while (node != null){
+            if (map.get(node.val) != null){
+                if (node.next == null){
+                    prev.next = null;
+                    break;
+                }else{
+                    prev.next = node.next;
+                }
+            }else{
+                prev = node;
+                map.put(node.val,new Object());
+            }
+            node = node.next;
+        }
+        return head;
     }
 
     public class ListNode {
@@ -15,5 +38,9 @@ public class Solution {
         ListNode(int x) {
             val = x;
         }
+    }
+
+    public static void main(String args[]){
+
     }
 }
